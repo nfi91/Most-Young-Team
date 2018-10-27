de7bb00c3ff0d5b8717a383aaf54a68070f5dea4
germandemiandemarco <germandemiandemarco@gmail.com>
2018-10-27 18:56:49 -0300

    upload

--- CityOfAaron/src/control/CropControl.java
+++ CityOfAaron/src/control/CropControl.java
@@ -1,103 +1,130 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+*The CropControl class - part of the control layer
+* class contains all of the calculation methods for managing the crops
+* Author: German De Marco
+* Date last modified: October 2018
+*/
+
 package control;
+
 import model.cityofaaron.CropData;
 import java.util.Random;
+
 /**
  *
  * @author de marco, german
  */
 public class CropControl {
-    
-//constants
-private static final int LAND_BASE = 17;
-private static final int LAND_RANGE = 10;
-  
-// random number generator
-private static Random random = new Random();
 
-    
+//constants
+    private static final int LAND_BASE = 17;
+    private static final int LAND_RANGE = 10;
+
+// random number generator
+    private static Random random = new Random();
+    private static int wheatInStore;
+
     /**
-     * calcLandCost method
-     * Purpose: To calculate a random land cost between 30 and 50 bushels/acre
-     * Par: None
-     * @return 
+     * calcLandCost method Purpose: To calculate a random land cost between 30
+     * and 50 bushels/acre Par: None
+     *
+     * @return
      */
-    public static int calcLandCost(){
-    
-int landPrice = random.nextInt(LAND_RANGE) + LAND_BASE;
-return landPrice;
+    public static int calcLandCost() {
+
+        int landPrice = random.nextInt(LAND_RANGE) + LAND_BASE;
+        return landPrice;
     }
-    
+
     /**
-     * The sellLand method
-     * Purpose: To sell land
+     * The sellLand method Purpose: To sell land
+     *
      * @param landPrice
      * @param acresToSell
      * @param cropData
-     * @return the number of acres
-     * Pre-Conditions: acres to sell must be positive
-     * and <= acresOwned
+     * @return the number of acres Pre-Conditions: acres to sell must be
+     * positive and <= acresOwned
      */
-   public static int sellLand(int landPrice, int acresToSell, CropData cropData)
-   {
-       //if  acresToSell < 0, return -1
-        if (acresToSell <0)
+    public static int sellLand(int landPrice, int acresToSell, CropData cropData) {
+        //if  acresToSell < 0, return -1
+        if (acresToSell < 0) {
             return -1;
-       //if acresToSell > acresOwned, return -1
-       int acresOwned = cropData.getAcresOwned();
-       if (acresToSell > acresOwned)
-           return -1;
-       // acresOwned = acresOwned - acresToSell
-       acresOwned -= acresToSell;
-       cropData.setAcresOwned(acresOwned);
-       //whetInStore = wheatInStore + acresToSell * 
-       int wheatInStore = cropData.getWheatInStore();
-       wheatInStore += (acresToSell - landPrice);
-       cropData.setWheatInStore(wheatInStore);
-       //return acresOwned
-       return acresOwned;
-       
-       
-   }
-   
-  /**
-   * The BuyLand method
-   * Purpose Buy land
-   * @param landPrice
-   * @param acresToBuy
-   * @param cropData
-   * @return the acres owned.
-   * * Precondition: acres to buy must be positive
-   * and <= acresOwned
-   */
-   
-  
- public static int buyLand(int landPrice, int acresToBuy, CropData cropData, int wheatAfterBuyAcres)
-    {
-     
-// if acresTobuy < 0, return 1
-     if (acresToBuy < 0)
-         return -1;
-     //
-int wheatInStore = cropData.getWheatInStore();     
-if (wheatInStore < landPrice)
-         return 1;
-//acresOwned = acresOwned + acresToBuy
-int acresOwned = cropData.getAcresOwned();     
-    
-    acresOwned += acresToBuy;
-    cropData.setAcresOwned(acresToBuy);
-     
-//wheatAfterBuyAcress = wheatInStore – (acresOwned + acresToBuy)
-wheatAfterBuyAcres = wheatInStore – (acresOwned + acresToBuy)
-cropData.setWheatInStore(wheatInStore);
-    //return acresOwned
-    return acresOwned;
-    }
-   
-}
+        }
+        //if acresToSell > acresOwned, return -1
+        int acresOwned = cropData.getAcresOwned();
+        if (acresToSell > acresOwned) {
+            return -1;
+        }
+        // acresOwned = acresOwned - acresToSell
+        acresOwned -= acresToSell;
+        cropData.setAcresOwned(acresOwned);
+        //whetInStore = wheatInStore + acresToSell * 
+        int wheatInStore = cropData.getWheatInStore();
+        wheatInStore += (acresToSell * landPrice);
+        cropData.setWheatInStore(wheatInStore);
+        //return acresOwned
+        return acresOwned;
 
+    }
+
+    /**
+     * The BuyLand method Purpose: Show the final amount of land
+     *
+     * @param landPrice
+     * @param acresToBuy
+     * @param cropData
+     * @return the number of acres after the operations Pre-Conditions: acres to
+     * sell must be <= acresOwned
+     */
+    public static int buyLand(int landPrice, int acresToBuy, CropData cropData) {
+
+// if acresTobuy < 0, return -1
+        if (acresToBuy < 0) {
+            return -1;
+        }
+
+//if wheatInStore < acresToBuy * landPrice, return -1
+        int wheatAmount = cropData.getWheatInStore();
+        if (wheatAmount < acresToBuy * landPrice) {
+            return -1;
+        }
+
+//acresOwned = acresOwned + acresToBuy
+        int acresOwned = cropData.getAcresOwned();
+
+        acresOwned = acresOwned + acresToBuy;
+        cropData.setAcresOwned(acresOwned);
+
+//wheatAfterBuyAcres = wheatInStore – (acresOwned + acresToBuy)
+        wheatAmount = (acresOwned + acresToBuy * landPrice);
+        cropData.setWheatInStore(wheatAmount);
+        return wheatAmount;
+
+    }
+
+    /**
+     * SetOffering Method Purpose: Show valid values for offerings.
+     *
+     * @param offering_Payment
+     * @param cropdata
+     * @return
+     */
+    public static int setOffering(int offering_Payment, CropData cropData) {
+
+        //if offering < 1 %, Return -1 
+        if (offering_Payment < 1) {
+
+            return -1;
+        }
+
+        //if offering > 99 % Return – 1
+        if (offering_Payment > 99) {
+            return -1;
+        }
+
+        cropData.setOffering(offering_Payment);
+        return offering_Payment;
+
+    }
+
+}
--- CityOfAaron/test/control/CropControlTest.java
+++ CityOfAaron/test/control/CropControlTest.java
@@ -20,48 +20,190 @@ public class CropControlTest {
 
     /**
      * Test of calcLandCost method, of class CropControl.
+     * Test Case Buy Land - 1
      */
+    
     @Test
-    public void testCalcLandCost() {
-        System.out.println("calcLandCost");
-        int expResult = 0;
-        int result = CropControl.calcLandCost();
+  public void testBuyLand() {
+        System.out.println("buyLand Test Case 1");
+        int landPrice = 10;
+        int acresToBuy = 50;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(1500);
+        cropData.setWheatInStore(700);
+        int expResult = 300;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+      
     }
-
     /**
      * Test of sellLand method, of class CropControl.
-     */
+     * Test Case Buy Land - 2 **/
+   
+    
     @Test
-    public void testSellLand() {
-        System.out.println("sellLand");
-        int landPrice = 20;
-        int acresToSell = 10;
+ public void testBuyLand2() {
+        System.out.println("buyLand Test Case 2");
+        int landPrice = 25;
+        int acresToBuy = -8;
         CropData cropData = new CropData();
-        cropData.setAcresOwned(2800);
-        int expResult = 2790;
-        int result = CropControl.sellLand(landPrice, acresToSell, cropData);
+        cropData.setAcresOwned(1500);
+        cropData.setWheatInStore(700);
+        int expResult = -1;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+      
     }
 
     /**
      * Test of buyLand method, of class CropControl.
-     */
+     Test Case Buy Land - 3 **/
     @Test
-    public void testBuyLand() {
-        System.out.println("buyLand");
-        int landPrice = 0;
-        int acresToBuy = 0;
-        CropData cropData = null;
+    public void testBuyLand3() {
+        System.out.println("buyLand Test Case 3");
+        int landPrice = 50;
+        int acresToBuy = 800;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(1500);
+        cropData.setWheatInStore(700);
+        int expResult = -1;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }
+    
+    /**
+     * Test of buyLand method, of class CropControl.
+     Test Case Buy Land - 4 **/
+    
+    
+    @Test
+    public void testBuyLand4() {
+        System.out.println("buyLand Test Case 4 ");
+        int landPrice = 30;
+        int acresToBuy = 3;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(1500);
+        cropData.setWheatInStore(200);
         int expResult = 0;
         int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+      
     }
     
+     /**
+     * Test of buyLand method, of class CropControl.
+     Test Case Buy Land - 5 **/
+    
+    
+   @Test 
+   public void testBuyLand5() {
+        System.out.println("buyLand");
+        int landPrice = 30;
+        int acresToBuy = 0;
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(1500);
+        cropData.setWheatInStore(200);
+        int expResult = 0;
+        int result = CropControl.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+      
+    }  
+    
+ /**
+     * Test of SetOffering method, of class CropControl.
+     Test Case SetOffering - 1 **/
+   
+   @Test
+   public void testSetOffering1() {
+       
+     System.out.print("Offering Test 1");
+     int offering_Payment = 5;
+     CropData cropData = new CropData();
+     cropData.setOffering(offering_Payment);
+     int expResult = 5;
+     int result = CropControl.setOffering(offering_Payment, cropData);
+     assertEquals(expResult, result);
+     
+     
+     
+   }
+ /**
+     * Test of SetOffering method, of class CropControl.
+     Test Case SetOffering - 2 **/
+     @Test
+   public void testSetOffering2() {
+       
+     System.out.print("Offering Test 2");
+     int offering_Payment = 0;
+     CropData cropData = new CropData();
+     cropData.setOffering(offering_Payment);
+     int expResult = -1;
+     int result = CropControl.setOffering(offering_Payment, cropData);
+     assertEquals(expResult, result);
+     
+     
+     
+   }
+    /**
+     * Test of SetOffering method, of class CropControl.
+     *  Test Case SetOffering - 3 **/
+   
+    @Test
+   public void testSetOffering3() {
+       
+     System.out.print("Offering Test 4");
+     int offering_Payment = -5;
+     CropData cropData = new CropData();
+     cropData.setOffering(offering_Payment);
+     int expResult = -1;
+     int result = CropControl.setOffering(offering_Payment, cropData);
+     assertEquals(expResult, result);
+    
+     
+   }
+   
+    /**
+     * Test of SetOffering method, of class CropControl.
+     *  Test Case SetOffering - 4 **/
+   
+   @Test
+   public void testSetOffering4() {
+       
+     System.out.print("Offering Test 4");
+     int offering_Payment = 1;
+     CropData cropData = new CropData();
+     cropData.setOffering(offering_Payment);
+     int expResult = 1;
+     int result = CropControl.setOffering(offering_Payment, cropData);
+     assertEquals(expResult, result);
+     
+     
+     
+   } 
+    
+    /**
+     * Test of SetOffering method, of class CropControl.
+     *  Test Case SetOffering - 5 **/
+   
+   public void testSetOffering5() {
+       
+     System.out.print("Offering Test 5");
+     int offering_Payment = 99;
+     CropData cropData = new CropData();
+     cropData.setOffering(offering_Payment);
+     int expResult = 99;
+     int result = CropControl.setOffering(offering_Payment, cropData);
+     assertEquals(expResult, result);
+     
+     
+     
+   } 
 }
+
+
